47e57c14ec1ee9874e9e485254ebbe8a5558e9f1
Ulrich Eck <ulrich.eck@tum.de>
2020-04-26 21:15:43 +0200

    adopted to camp conan env

--- conanfile.py
+++ conanfile.py
@@ -14,15 +14,15 @@ class PythonPipConan(ConanFile):
         tools.get("https://github.com/pypa/pip/archive/%s.tar.gz" % self.version)
 
     def build_requirements(self):
-        self.build_requires("generators/1.0.0@%s/stable" % self.user)
-        self.build_requires("python-setuptools/[>=41.2.0]@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@camposs/stable")
+        self.build_requires("python-setuptools/[>=41.2.0]@camposs/stable")
 
     def requirements(self):
-        self.requires("python/[>=3.7.4]@%s/stable" % self.user)
+        self.requires("python/[>=3.8.2]@camposs/stable")
 
     def build(self):
         with tools.chdir("pip-" + self.version):
             self.run('python setup.py install --optimize=1 --prefix= --root="%s"' % self.package_folder)
 
     def package_info(self):
-        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.7", "site-packages"))
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.8", "site-packages"))
